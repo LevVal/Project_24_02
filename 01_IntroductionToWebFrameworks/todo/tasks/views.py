2a907a6d5707136fad8a5fea60383bb4136b90f2
Valeriya Levitina <levitina.v@yandex.ru>
2022-11-26 18:31:52 +0300

    done

--- 01_IntroductionToWebFrameworks/todo/tasks/views.py
+++ 01_IntroductionToWebFrameworks/todo/tasks/views.py
@@ -1,14 +1,18 @@
 from django.http import HttpResponse
 
 from django.views import View
-
+import random
 
 class ToDoView(View):
-
     def get(self, request, *args, **kwargs):
-        return HttpResponse('<ul>'
-                            '<li>Установить python</li>'
-                            '<li>Установить django</li>'
-                            '<li>Запустить сервер</li>'
-                            '<li>Порадоваться результату</li>'
-                            '</ul>')
+        to_do = ''
+        deal = ['<li>Поспать</li>',
+                 '<li>Поесть</li>',
+                 '<li>Почистить зубы</li>',
+                 '<li>Потанцевать</li>',
+                 '<li>Поработать</li>']
+
+        for i_deal in range(5):
+            to_do += deal[random.randint(0, 4)]
+
+        return HttpResponse('<ul>' + to_do + '</ul>')
